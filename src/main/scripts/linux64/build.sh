a35432ae3834b6f3b16797981c9aa30329d7f711
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2020-04-22 18:38:21 +0200

    Update build.sh

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,1 +1,14 @@
-return 0
+#!/usr/bin/env bash
+
+cmake . \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" \
+-DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+
+make
+r1=$?
+
+mkdir -p ../../../../target/classes/linux64/
+cp Play50hz-player.bin ../../../../target/classes/linux64/Play50hz-player.bin
+
+exit ${r1}
