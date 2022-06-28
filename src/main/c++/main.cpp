5d5e5a2b260be855ec9af5bc262ff4f94c93cf0d
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2022-06-28 19:34:58 +0200

    [FEATURE] Support java 19

--- src/main/c++/main.cpp
+++ src/main/c++/main.cpp
@@ -96,7 +96,7 @@ std::string workingdir() {
 }
 
 int runApp() {
-    std::string cmd = "\"" + workingdir() +  "/" + javaFile + "\"" + " -jar --add-modules=jdk.incubator.foreign --enable-native-access=ALL-UNNAMED play50hz-player.jar";
+    std::string cmd = "\"" + workingdir() +  "/" + javaFile + "\"" + " -jar --enable-preview --enable-native-access=be.yildizgames.retro.player.emulator.libretro,be.yildizgames.module.controller.sdl play50hz-player.jar";
     return system(cmd.c_str());
 }
 	
