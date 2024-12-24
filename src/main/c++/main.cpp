bee57bac75ccb7cbadd1cf9c4924a30c46f10c47
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2024-12-24 21:25:04 +0100

    Update main.cpp

--- src/main/c++/main.cpp
+++ src/main/c++/main.cpp
@@ -112,7 +112,7 @@ std::string workingdir() {
 }
 
 int runApp() {
-    std::string cmd = "\"" + workingdir() +  "/" + javaFile + "\"" + " -jar --enable-native-access=ALL-UNNAMED,be.yildizgames.module.compression.sevenzip,be.yildizgames.module.controller.sdl,be.yildizgames.retro.player.emulator play50hz-player.jar";
+    std::string cmd = "\"" + workingdir() +  "/" + javaFile + "\"" + " -jar play50hz-player.jar";
     return system(cmd.c_str());
 }
 	
